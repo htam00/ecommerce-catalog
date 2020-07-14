531b19c0980358d46151ec34f196e82dec092389
htam00 <anonimozurik@gmail.com>
2020-07-14 01:53:24 -0300

    add mongodb & connected database

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"net/http"
+	"context"
 	"fmt"
 	"log"
 	"time"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,10 +28,17 @@ func ProductsIDHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://matheus:zurik21@ds237989.mlab.com:37989/ecommerce_catalog"))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(ctx)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", IndexHandler)
 	r.HandleFunc("/products", ProductsHandler)
-	r.HandleFunc("/products/{id}", ProductsIDHandler
+	r.HandleFunc("/products/{id}", ProductsIDHandler)
 	http.Handle("/", r)
 
 	srv := &http.Server {
