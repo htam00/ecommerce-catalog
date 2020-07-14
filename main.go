0d60b076bbd264a6bd4ceb561245bd146c2ae0e4
htam00 <anonimozurik@gmail.com>
2020-07-14 14:28:10 -0300

    insert one product in database

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,12 +29,42 @@ func ProductsIDHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	// Type Product
+	type Product struct {
+	 ID	primitive.ObjectID `bson:"_id,omitempty"`
+	 Name	string	`bson:"name,omitempty"`
+	 Description	string `bson:"description,omitempty"`
+	 Price	int	`bson:"price,omitempty"`
+	}
+
+	// Connect Database
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://matheus:zurik21@ds237989.mlab.com:37989/ecommerce_catalog"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://matheus:zurik21@ds237989.mlab.com:37989/ecommerce_catalog?retryWrites=false"))
 	if err != nil {
 		panic(err)
 	}
 	defer client.Disconnect(ctx)
+
+	// Database Name
+	database := client.Database("ecommerce_catalog")
+
+	// Instance Product
+	product := Product{
+		Name: "Frauda",
+		Description: "Fraudas descartaveis Tam: P,M,G",
+		Price: 10,
+	}
+
+	// Collection Name
+	productsCollection := database.Collection("products")
+
+	// Insert One Product
+	insertResult, err := productsCollection.InsertOne(ctx, product)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(insertResult.InsertedID)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", IndexHandler)
